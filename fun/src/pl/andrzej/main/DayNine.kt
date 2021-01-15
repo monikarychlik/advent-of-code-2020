3ec07e63ef7bb0afda50e6e22745e4fca011586c
Monika Rychlik <rychlik.monikaa@o2.pl>
2021-01-15 21:36:01 +0100

    Add files via upload

---
+++ fun/src/pl/andrzej/main/DayNine.kt
@@ -0,0 +1,18 @@
+package pl.andrzej.main
+
+class DayNine {
+
+    companion object {
+
+        private val input = listOf("")
+
+        fun firstPuzzle() {
+            println("Result: ")
+        }
+
+        fun secondPuzzle() {
+
+            println("Result: ")
+        }
+    }
+}
---
+++ fun/src/pl/andrzej/main/Main.kt
@@ -0,0 +1,11 @@
+package pl.andrzej.main
+
+class Main {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            DayNine.firstPuzzle()
+        }
+    }
+}
